e1ce03ce2fbd0536f0afa3cb78b9ebe11af66b9c
oshanAmarasuriya <oshana.20@cse.mrt.ac.lk>
2025-01-25 16:24:04 +0000

    Add Persona to the main_workflow

--- workflows/main_workflow.py
+++ workflows/main_workflow.py
@@ -2,7 +2,7 @@ from agents.keyword_extraction_agent import KeywordExtractionAgent
 from agents.retrieval_agent import RetrievalAgent
 from agents.question_answering_agent import QuestionAnsweringAgent
 from agents.manager_agent import ManagerAgent
-
+from agents.persona_agent import PersonaAgent
 from langgraph.graph import StateGraph, START
 from schemas.agents import AgentState
 
@@ -12,12 +12,14 @@ graph = StateGraph(AgentState)
 # Add agent nodes
 graph.add_node("Manager", ManagerAgent.manageMainFlow)
 graph.add_node("KEA", KeywordExtractionAgent.extract_keywords)
+graph.add_node("Persona", PersonaAgent.talk)
 graph.add_node("RA", RetrievalAgent.retrieve_documents)
 graph.add_node("QAA", QuestionAnsweringAgent.answer_question)
 
 # Define edges to establish the workflow
 graph.add_edge(START, "Manager")
 graph.add_edge("KEA", "Manager")
+graph.add_edge("Persona", "Manager")
 graph.add_edge("RA", "Manager")
 graph.add_edge("QAA", "Manager")
 
