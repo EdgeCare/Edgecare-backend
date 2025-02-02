ced3a80904826d92451bf9e188304fceec010623
oshanAmarasuriya <oshana.20@cse.mrt.ac.lk>
2025-02-02 11:55:56 +0000

    Integrate persona agent

--- agents/manager_agent.py
+++ agents/manager_agent.py
@@ -5,10 +5,14 @@ from schemas.agents import AgentState
 
 class ManagerAgent:
     @staticmethod
-    def manageMainFlow(state: AgentState) -> Command[Literal["KEA", "RA", "QAA", END]]:
+    def manageMainFlow(state: AgentState) -> Command[Literal["KEA", "RA", "QAA","Persona", END]]:
         if not state.keywords:
             print("\n🕵️ Invoking Keyword Extraction Agent")
             return Command(goto="KEA")
+        if len(state.keywords) == 0:
+            # state.keywords.append('##NO_KEY_WORDS')
+            print("\n🕵️ Invoking persona for a casual reply")
+            return Command(goto="Persona")
         elif not state.documents:
             print("\n🕵️ Invoking Retrieval Agent")
             return Command(goto="RA")
@@ -16,9 +20,10 @@ class ManagerAgent:
             print("\n🕵️ Invoking Question Answering Agent")
             return Command(goto="QAA")
         else:
-            print("\nProcess Complete!\n")
-            # print("state =", state)
-            return Command(goto=END)
+            print("\n🕵️ Invoking persona for a medical reply")
+            return Command(goto="Persona")
+
+
 
     @staticmethod
     def manageMcqQuestions(state: AgentState) -> Command[Literal["KEA", "RA", "MCQA", END]]:
