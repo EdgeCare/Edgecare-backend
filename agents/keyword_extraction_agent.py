7f84f986ee29d5cea2cdb87a1469ddae6dc0d60a
ThejanB <thejanw.20@cse.mrt.ac.lk>
2025-02-04 15:54:13 +0000

    keyword extracrion agent updated

--- agents/keyword_extraction_agent.py
+++ agents/keyword_extraction_agent.py
@@ -2,38 +2,65 @@ from pydantic import BaseModel
 from typing import List, Optional
 from schemas.agents import AgentState
 from utils.openaiClient import openaiClient
-from utils.extractContent import extract_list
+from utils.extractContent import extract_list, extract_json
 
 class KeywordExtractionAgent:
     @staticmethod
     def extract_keywords(state: AgentState) -> dict:
         print("🤖 Keyword Extraction Agent Running")
-        userQuestion = state.user_query
 
-        prompt = f'''
-        You are an expert medical knowledge assistant. Your task is to generate specific and concise queries for retrieving the most relevant medical documents based on a user's question. 
+        prompt = """You are Lisa, a medical assistant designed to extract key medical terms from user conversations. Your primary role is to identify relevant medical keywords for retrieval purposes and determine whether a conversation is medical-related. You must classify user input into one of three categories:
+1. Medical-related inquiry that requires snippet retrieval
+2. Medical-related inquiry that does not require snippet retrieval (like a greeting, or if ask what can you do?)
+3. Non-medical inquiry (unsupported)
 
-        The generated queries should:
-        1. Focus on the key medical concepts, terms, and conditions mentioned in the question.
-        2. Include synonyms or related terms for broader coverage if necessary.
-        3. Be formatted to maximize retrieval effectiveness from a medical document database.
-        4. Use professional terminology where applicable.
+Instructions:
+1. If the conversation is medical-related and requires retrieval (UNSUPPORTED = False, RETRIEVE_SNIPPETS = True, KEYWORDS: [...]):
+
+- Extract key medical concepts, conditions, symptoms, treatments, and terms.
+- Include synonyms or related terms for broader retrieval effectiveness.
+- Use professional medical terminology where applicable.
+- Generate 5 optimized queries
+- Return the queries in a Python list format.
+
+2. If the conversation is medical-related but does not require retrieval (UNSUPPORTED = False, RETRIEVE_SNIPPETS = False, MESSAGE = "..."):
+- Example: General medical advice like "How can you help me?" doesn't require retrieval but is still a medical conversation related to us.
+- If the user greets you or asks what you do, respond with an introduction.
+
+3. If the conversation is not medical-related (UNSUPPORTED = True, MESSAGE = "..."):
+- Politely inform the user that you handle only medical-related inquiries.
+- Example: If the user asks about coding, writing essays, or other non-medical topics, return "UNSUPPORTED": True with a polite explanation that you are medical assistance.
+
+Response Format (Always JSON:
+- Your can include these fields:
+    {
+        "UNSUPPORTED": false,
+        "RETRIEVE_SNIPPETS": true,
+        "MESSAGE": "reason for unsupported" or "reply to greeting requests" 
+        "KEYWORDS": ["python list of extracted keywords"]
+    }
+    "UNSUPPORTED" → true if the query is non-medical; otherwise, false.
+    "RETRIEVE_SNIPPETS" → true if retrieval should happen; false otherwise.
+    "KEYWORDS" → Extracted medical queries as a Python list (if RETRIEVE_SNIPPETS = true)."""
         
-        
-        User Question: "{userQuestion}"
-
-        Generate 3-5 optimized queries for retrieving relevant medical documents. 
-        Provide the queries as a Python list of strings. Ensure the list format is valid and ready for parsing directly into a Python program.
-        Do not give any query if the user question is not about a medical condition. Just give an empty list instead.
-        '''
+        if state.chat:
+            prompt += """
+--------------------
+Past Chat: """ + state.chat + """
+""" 
+        # add user question
+        prompt += """
+--------------------
+New query from user: """ + state.user_query + """
+"""
 
         for i in range (5):
-            keyWords =  openaiClient.generate_response(prompt)
-            extracted_keywords = extract_list(keyWords)
-            # extracted_keywords = ['body anatomy', 'human body structure', 'body composition', 'physiology of the human body', 'organ systems in the body']
-            if(len(extracted_keywords)>2):
+            reply =  openaiClient.generate_response(prompt)
+            extracted_json = extract_json(reply)
+            # extracted_json = ['body anatomy', 'human body structure', 'body composition', 'physiology of the human body', 'organ systems in the body']
+            if(len(extracted_json)>1):
                 break
 
-        print(f"extracted ekywords -> {extracted_keywords}")
+        print(f"extracted keywords -> {extracted_json}")
 
-        return {"keywords": extracted_keywords}
+        return {"keywords": ['body anatomy', 'human body structure', 'body composition', 'physiology of the human body', 'organ systems in the body']}
